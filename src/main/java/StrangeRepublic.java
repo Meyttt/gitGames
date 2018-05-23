81d66273eecc43e9b6e0536894f645a3a4b5548a
Meyttt <sabermeyt@gmail.com>
2018-05-23 14:16:12 +0300

    Communication skills improved

--- src/main/java/StrangeRepublic.java
+++ src/main/java/StrangeRepublic.java
@@ -2,4 +2,7 @@ public class StrangeRepublic {
     public static void makePeaceNotWar(){
         System.out.println("Peace!");
     }
+    public static Object industry(){
+        return "smth";
+    }
 }
