07eabaecff118024e36127f90a5113af749f30d8
Meyttt <sabermeyt@gmail.com>
2018-05-23 14:12:06 +0300

    Communication skills added

--- src/main/java/StrangeRepublic.java
+++ src/main/java/StrangeRepublic.java
@@ -1,5 +1,10 @@
+import java.util.Random;
+
 public class StrangeRepublic {
     public static void makePeaceNotWar(){
         System.out.println("Peace!");
+        System.out.println("We want to be friends with you!");
+        String number = String.valueOf(new Random().nextInt(1000000));
+        System.out.println("Call us! Our number: "+number);
     }
 }
